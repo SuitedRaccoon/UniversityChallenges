2a6baecd3bd8e3a774742c75115ac9a15aeb6810
Gustavo Saud <gustavosaud@gmail.com>
2026-04-23 20:02:07 -0300

    adição base da atividade da semana

---
+++ trabalhos-cpp/operador.cpp
@@ -0,0 +1,24 @@
+/*
+    Escreva um algoritmo que solicite ao usuario dois numeros reais e apresente:
+    1) a soma entre eles;
+    2) a subtracao do primeiro pelo segundo;
+    3) a multiplicacao entre eles;
+    4) a divisao do primeiro pelo segundo;
+    5) o resto da divisao do primeiro pelo segundo
+    6) a exponencial do primeiro valor (base) pelo segundo (expoente) (usar o for)
+    7) implementar recursividade no item 6 acima
+*/
+
+#include <stdio.h>
+
+float Somar(){
+
+}
+
+float Subtrair(){}
+float Multiplicar(){}
+float Dividir(){}
+float Resto(){}
+float Potencia(){}
+
+int main(){return 0;}
